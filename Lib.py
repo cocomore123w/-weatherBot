964327d34c5a6c5c9c3b755df44701cfcb0aed34
cocomore123w <86354672+cocomore123w@users.noreply.github.com>
2022-12-14 02:41:34 +0800

    update webhook push

--- Lib.py
+++ Lib.py
@@ -5,7 +5,7 @@ token = ''
 channelID =
 ##################
 
-##
+##   bot push
 def push(ctx):
     channel_id = channelID  # 頻道ID
     _token = "Bot "+ token  # 以Bot發送開頭須加上Bot {token}
@@ -22,4 +22,19 @@ def push(ctx):
     )
     return post.ok
 
+##   webhook push
+'''
+def push(ctx):
+    webhookURL=""
+    post = requests.post(
+        url=webhookURL,
+        headers={
+            "Content-Type": "application/json"
+        },
+        json={
+            "content": ctx
+        }
+    )
+    return post.ok
+'''
 #print(push())
